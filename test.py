5d0f98e5f869a36c6dafb057c45d44711724c4b8
Yufan You <ouuansteve@gmail.com>
2020-11-14 18:42:03 +0800

    tests: add an option to update the expected output
    This can be used on behavior changes or be used to see the diff of the
    actual output and the expected output via git diff.

--- test.py
+++ test.py
@@ -1,12 +1,26 @@
 import sys
+import argparse
 import pacwatch
 from io import StringIO
 from pathlib import Path
 
+expected_output = Path('tests') / 'data' / 'expected-output'
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--update', action='store_true',
+                        help='update expected output')
+    parserResult = parser.parse_args()
+
     pacwatch.settings['pacman_command'] = 'python tests/fakePacman.py'
-    output = StringIO()
-    sys.stdout = output
-    pacwatch.main()
-    with (Path('tests') / 'data' / 'expected-output').open('r') as expected:
-        assert output.getvalue() == expected.read()
+
+    if parserResult.update:
+        with expected_output.open('w') as f:
+            sys.stdout = f
+            pacwatch.main()
+    else:
+        output = StringIO()
+        sys.stdout = output
+        pacwatch.main()
+        with expected_output.open('r') as expected:
+            assert output.getvalue() == expected.read()
